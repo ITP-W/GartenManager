33989a3f14e5ad61ef99eb4779a464cd8f214286
A.W <71868337+AImperator@users.noreply.github.com>
2023-08-19 02:52:23 +0200

    [GM-A-0.0.0] FEATURE BASECODE
    New Feature = general and basic app function
    -
    Added Base Code for the app generated by VisualSudio2019

---
+++ GartenManager/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using GartenManager.Models;
+using GartenManager.ViewModels;
+
+namespace GartenManager.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
